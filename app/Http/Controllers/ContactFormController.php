8fc8f68f1912e7db961d6b78ffccab1fb60296da
Esmail Khalifa <hossainmdismail9010@gmail.com>
2024-09-07 16:38:31 +0600

    up

--- app/Http/Controllers/ContactFormController.php
+++ app/Http/Controllers/ContactFormController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Http;
 
 class ContactFormController extends Controller
 {
@@ -10,10 +11,47 @@ class ContactFormController extends Controller
     {
         $request->validate([
             'name'      => 'required',
-            'email'     => 'required|email',
+            'number'   => $request->number ? 'required' : '',
+            'email'    => $request->email ? 'required|email' : '',
             'category'  => 'required',
             'message'   => 'required',
         ]);
+
+        // $data = [
+        //     'name'          => $request->name,
+        //     'number'        => $request->number,
+        //     'email'         => $request->email,
+        //     'category'      => $request->category,
+        //     'description'   => $request->message,
+        //     'type'          => 'web-support',
+        // ];
+
+        // try {
+        //     $url = app('domainName') . '/api/customer/store';
+
+        //     // Send the POST request with the $data
+        //     $response = Http::post($url, $data);
+
+        //     // Check if the response was successful
+        //     if ($response->successful()) {
+        //         // Dump and die (for debugging purposes) with the response as JSON
+        //         dd($response->json());
+        //     } else {
+        //         // Handle the case where the response was not successful (e.g., 4xx or 5xx status code)
+        //         return response()->json([
+        //             'error' => 'Failed to send data',
+        //             'status' => $response->status(),
+        //             'body' => $response->body(),
+        //         ], $response->status());
+        //     }
+        // } catch (\Exception $e) {
+        //     // Catch any exceptions (e.g., network issues, invalid URL) and return the error message
+        //     return response()->json([
+        //         'error' => 'An error occurred',
+        //         'message' => $e->getMessage()
+        //     ], 500);
+        // }
+
 
         return redirect()->route('thanks');
     }

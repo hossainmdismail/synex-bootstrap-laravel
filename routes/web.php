f79aadf40a121343c7eb4f5c50473dcac44b27ec
Esmail Khalifa <hossainmdismail9010@gmail.com>
2024-11-24 13:56:13 +0600

    ncpc

--- routes/web.php
+++ routes/web.php
@@ -33,7 +33,7 @@ Route::post('/form-submit', [ContactFormController::class, 'index'])->name('cont
 Route::get('/services', [ServiceController::class, 'services'])->name('services'); //Done
 Route::prefix('service')->group(function () {
     Route::get('/web-development', [ServiceController::class, 'service_web_dev'])->name('service.web-dev');
-    Route::get('/management-software', [ServiceController::class, 'service_software'])->name('service.software');
+    Route::get('/cloud', [ServiceController::class, 'service_software'])->name('service.software');
     Route::get('/ui-ux', [ServiceController::class, 'service_uiux'])->name('service.uiux');
     Route::get('/digital-marketing', [ServiceController::class, 'service_digital_marketing'])->name('service.digital-marketing');
     Route::get('/graphics-design', [ServiceController::class, 'service_graphics_design'])->name('service.graphics-design');
